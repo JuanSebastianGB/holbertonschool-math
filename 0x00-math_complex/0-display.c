4abeb9c05661d2589dd4b4321ebcd03745ff2d3d
JuanSebastianGB <3393@holbertonschool.com>
2021-08-07 23:29:21 +0000

    UPD: Adding some conditions to deal with cases

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -8,8 +8,12 @@
 
 void display_complex_number(complex c)
 {
-	if (c.im)
-		printf("%d + %di\n", c.re, c.im);
-	else
-		printf("%d\n", c.re);
+	printf("%.0f", (double)c.re);
+
+	if (c.im < 0)
+		printf(" - %.0fi\n", (double)c.im);
+	else if (!c.im)
+		putchar('\n');
+	else if (c.im)
+		printf(" + %.0fi\n", (double)c.im);
 }
