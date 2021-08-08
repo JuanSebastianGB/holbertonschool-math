4466c79001e4daf465c28dc6d7b504133aa03a3b
JuanSebastianGB <3393@holbertonschool.com>
2021-08-08 16:07:44 +0000

    UPL: Uploading heron header

---
+++ 0x01-math_sequence/heron.h
@@ -0,0 +1,23 @@
+#ifndef _HERON_H_
+#define _HERON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
+
+/**
+ * struct t_series - list of results in each iteration of Heron sqrt
+ * @elt: Result of the application of the serie
+ * @next: point to the next node
+ */
+
+typedef struct t_series
+{
+	float elt;
+	struct t_series *next;
+} t_cell;
+
+t_cell *heron(double p, double x0);
+
+#endif /* _HERON_H_ */
