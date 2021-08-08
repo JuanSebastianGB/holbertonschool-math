4f53d432aff850574ab28c5eef0c863411bf9725
JuanSebastianGB <3393@holbertonschool.com>
2021-08-08 21:29:06 +0000

    FIX: making several changes in replit and updating the repository

--- 0x01-math_sequence/0-heron.c
+++ 0x01-math_sequence/0-heron.c
@@ -3,24 +3,31 @@
 /**
  * *add_node - to add node at the start of the heron list
  * @head: Head list
- * @p: Nunmber to get sqrt
- * Return: Nothing - void
+ * @p: Number to get sqrt
+ * Return: head node
  */
 
 t_cell *add_node(t_cell **head, double p)
 {
-	t_cell *new, *next;
 
-	if (!head || !*head)
+	t_cell *new;
+
+	if (!head)
 		return (NULL);
-	new = malloc(sizeof(new));
-	if (new == NULL)
+
+	new = malloc(sizeof(t_cell));
+	if (!new)
+		return (NULL);
+
+	if (*head == NULL)/*First node*/
 	{
-		free(new);
-		return (NULL);
+		new->elt = 1;
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-	next = (*head)->next;
-	new->elt = 0.5 * (next->elt + p / next->elt);
+	/* The actual node is not the first one */
+	new->elt = 0.5 * ((*head)->elt + p / (*head)->elt);
 	new->next = *head;
 	*head = new;
 	return (new);
@@ -30,21 +37,12 @@ t_cell *add_node(t_cell **head, double p)
 t_cell *heron(double p, double x0)
 {
 	t_cell *head;
-	t_cell first = {x0, NULL};
+	int i = x0;
 
-	printf("Valor recibido = %f",x0);
-/*	int i = 1;*/
+	head = NULL;
 
-
-
-	head = &first;
-	add_node(&head, p);
-/*
-	while (i < 7)
-	{
+	for (; i <= x0 + 6; i++)
 		add_node(&head, p);
-		i++;
-	}
-*/
+
 	return (head);
-}
+} 
